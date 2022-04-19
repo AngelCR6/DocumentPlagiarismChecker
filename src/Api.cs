329ea81605bb9e94eb4ef05a0e7704f0da3780ea
edumargar <eduardmartinezgarrido@gmail.com>
2022-04-19 17:57:56 +0200

    Cambio TerminalOutputedu
    he cambiado TerminalOutput por TerminalOutputedu

--- src/Api.cs
+++ src/Api.cs
@@ -97,7 +97,7 @@ namespace DocumentPlagiarismChecker
         /// <param name="results">A set of file matching scores</param>
         public void WriteOutput(){
             //TODO: must be selected by settings
-            TerminalOutput t = new TerminalOutput(this.Settings);
+            TerminalOutputedu t = new TerminalOutputedu(this.Settings);
             t.Write(this.MatchingResults);
         }
 
